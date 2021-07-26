4f9808a514864f0b6614afe86dd3f25f684cd8c2
Dmity-kl <i6169696@gmail.com>
2021-07-27 01:18:44 +0300

    Updates

---
+++ precache-manifest.ee4e94b9d9d7c857bb49f5c1a8a09be3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3600d2a15bef86f3086cbc239e12f013",
+    "url": "/todoList/index.html"
+  },
+  {
+    "revision": "be80efc100e6b5a98366",
+    "url": "/todoList/static/css/main.7e033bad.chunk.css"
+  },
+  {
+    "revision": "d8b49394a4ef110c78b8",
+    "url": "/todoList/static/js/2.0f2e4cd6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todoList/static/js/2.0f2e4cd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be80efc100e6b5a98366",
+    "url": "/todoList/static/js/main.0f643785.chunk.js"
+  },
+  {
+    "revision": "c39c464575b971d22f9a",
+    "url": "/todoList/static/js/runtime-main.67bcfed6.js"
+  }
+]);
